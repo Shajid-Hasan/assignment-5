bba62d9ae961e4e367a16a524e25d78cc5365e91
Shajid <hasan.shajid222@gmail.com>
2025-08-29 23:04:31 +0600

    added new

--- operation.js
+++ operation.js
@@ -49,13 +49,21 @@ function setInnerTextCopy (value){
 
 const copyBtns = document.getElementsByClassName('copy-btn')
 for(let copy of copyBtns){
-    console.log(copy)
     copy.addEventListener('click', function(){
         const availableCopy = getAvilableCopy('copy-count')
         
         const totalCopy = availableCopy + 1
 
         setInnerTextCopy(totalCopy)
+
+        alert('নম্বর কপি হয়েছে 999')
+
+        // document.getElementById('copy-btn').addEventListener('click', function(){
+        //     const numberText = document.getElementById('third').innerText
+        //     navigator.clipboard.writeText(numberText).then(() => {
+        //     alert("Copied number: " + numberText);
+        // })
+        
     })
 }
 
@@ -76,11 +84,13 @@ for(let heart of heartIcon){
 // Call button secction
 
 const cartBtns = document.getElementsByClassName('cart-btn')
+console.log(cartBtns)
+
 for(let cartButton of cartBtns){
     cartButton.addEventListener('click', function(){
-        alert('Calling National emergency service 999..')
-        const callBtn1 = cartButton.parentNode.parentNode.children[1].children[0]
-        const callBtn2 = cartButton.parentNode.parentNode.children[1].children[2]
+        alert('📞 Calling National emergency service 999..')
+        const callBtn1 = cartButton.parentNode.parentNode.children[1].children[0].innerText
+        const callBtn2 = cartButton.parentNode.parentNode.children[1].children[2].innerText
         
         console.log(callBtn1,callBtn2)
         
@@ -92,7 +102,7 @@ for(let cartButton of cartBtns){
         const totalAvailableCoin = availableCoins - 20
         
         if(totalAvailableCoin == 0){
-            alert('not enough coin for call')
+            alert('❌ আপনার পর্যাপ্ত কয়েন নেই, কল করতে ২০ কয়েন লাগবে')
         }
 
         const now = new Date();
@@ -101,13 +111,13 @@ for(let cartButton of cartBtns){
 
         // Asside Section
 
-        const cartHistory = getElement('history-section')
+        const cartHistory = getElement('clear-history')
         
         const newCart = document.createElement('div');
         newCart.innerHTML = `
             <div class="bg-[#fafafa] shadow-sm my-5 p-4 flex justify-between">
-                    <div><h4 class="text-[13px] text-[#111111] my-1">জাতীয় জরুরি সেবা</h4>
-                    <p class="text-[10px] text-[#111111]">999</p>
+                    <div><h4 class="text-[13px] text-[#111111] my-1">${callBtn1}</h4>
+                    <p class="text-[10px] text-[#111111]">${callBtn2}</p>
                     </div>
                     <div><p id="clock">${now.toLocaleTimeString()}</p></div>
 
@@ -122,8 +132,10 @@ for(let cartButton of cartBtns){
 // clear Button
 
 document.getElementById('clear-btn').addEventListener('click', function(){
-    const cartHistory = getElement('history-section')
-    cartHistory.innerHTML = ''
+    console.log('click clear btn')
+
+    document.getElementById('clear-history').innerHTML = ''
+    
 })
 
 
--- time.js
+++
@@ -1,8 +0,0 @@
-
-// function updateClock() {
-//         const now = new Date();
-//         document.getElementById("clock").textContent = now.toLocaleTimeString();
-//     }
-
-//     updateClock();
-//     setInterval(updateClock, 1000);
